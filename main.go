9535c40b84240fa39e68e175d408421b8a4bdfc6
johnstonjacob <johnstonajacob@gmail.com>
2018-08-30 02:00:01 +0000

    runner client init

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func runner_client(n string) (res int, err error) {
+
+	MQ_HOST := os.Getenv("MQ_HOST")
+
+	conn, err := amqp.Dial(MQ_HOST)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	err = ch.Publish(
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(n),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrId == d.CorrelationId {
+			break
+		}
+	}
+
+	return
+}
+
+func main() {
+	n := bodyFrom(os.Args)
+
+	log.Printf(" [x] Requesting output for %s", n)
+	res, err := runner_client(n)
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %d", res)
+}
+
+func bodyFrom(args []string) string {
+	return strings.Join(args[1:], " ")
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
