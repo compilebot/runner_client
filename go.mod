7b2c27a1ef16fa10dd05d11af1681f0d9c25537e
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-30 19:44:55 -0500

    testing init, final client setup

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/gopherpun/runner_client
 
 require (
+	github.com/franela/goblin v0.0.0-20180624194345-6af5ecb776ce
 	github.com/go-chi/chi v3.3.3+incompatible // indirect
 	github.com/streadway/amqp v0.0.0-20180806233856-70e15c650864
+	golang.org/x/tools v0.0.0-20180830214440-4bc20fc7916b // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,108 @@
 package main
 
 import (
-	"chi"
-	"http"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
 )
 
+func runnerClient(n string) (res string, err error) {
+
+	mqHost := os.Getenv("MQ_HOST")
+
+	conn, err := amqp.Dial(mqHost)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrID := randomString(32)
+
+	err = ch.Publish(
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrID,
+			ReplyTo:       q.Name,
+			Body:          []byte(n),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		if corrID == d.CorrelationId {
+			res = string(d.Body)
+			break
+		}
+	}
+
+	return
+}
+
 func main() {
-	r := chi.NewRouter()
+	n := `
+	package main
 
-	r.Post("/api/compile/go", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	import "fmt"
 
-	http.ListenAndServe(":3000", r)
+	func main() {
+		fmt.Println("hello world")
+	}
+	`
+
+	log.Printf(" [x] Requesting output for %s", n)
+	res, err := runnerClient(n)
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %s", res)
+}
+
+func bodyFrom(args []string) string {
+	return strings.Join(args[1:], " ")
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
 }
